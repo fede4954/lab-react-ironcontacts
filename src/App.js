48e633584bd0f855764ad8043c797c7026fa477d
Fede <f.gomezlara@outlook.es>
2021-11-30 19:40:49 +0100

    iteration 2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ const contactsArray = contacts.splice(5)
 const App = () => {
   return <div className="App">
     <h1>IronContacts</h1>
+
     <table>
       <tr>
         <th>Picture</th>
         <th>Name</th>
         <th>Popularity</th>
+        <th>Won an Oscar</th>
+        <th>Won an Emmy</th>
       </tr>
+
       {contactsArray.map((person, index) => {
-        const { pictureUrl, name, popularity } = person
+        const { pictureUrl, name, popularity, wonOscar, wonEmmy } = person
         return (
           <tr key={index + Date.now()}>
             <td><img src={pictureUrl} alt={name} /></td>
             <td>{name}</td>
             <td>{popularity}</td>
+            
+            {wonOscar ? <td>🏆</td> : <td></td>}
+            {wonEmmy ? <td>🏆</td> : <td></td>}
           </tr>
         )
       })}
