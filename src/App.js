173d2cbecb299f5d8628934a4ee4b6cafe96a488
Fede <f.gomezlara@outlook.es>
2021-11-30 19:28:58 +0100

    iteration 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import contacts from './contacts.json'
+const contactsArray = contacts.splice(5)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <div className="App">
+    <h1>IronContacts</h1>
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      {contactsArray.map((person, index) => {
+        const { pictureUrl, name, popularity } = person
+        return (
+          <tr key={index + Date.now()}>
+            <td><img src={pictureUrl} alt={name} /></td>
+            <td>{name}</td>
+            <td>{popularity}</td>
+          </tr>
+        )
+      })}
+    </table>
+  </div>
 }
 
-export default App;
+export default App
